7f71c7857415db58c2509637f3e5e8cae568179d
craycentel <craycentel@gmail.com>
2016-07-02 12:37:50 +0300

    Add
    Add

---
+++ JavaRushHomeWork/src/com/javarush/test/level03/lesson12/home03/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level03.lesson12.home03;
+
+/* Я буду зарабатывать $50 в час
+Ввести с клавиатуры число n.
+Вывести на экран надпись «Я буду зарабатывать $n в час».
+Пример:
+Я буду зарабатывать $50 в час
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String number = bufferedReader.readLine();
+        System.out.print("Я буду зарабатывать $" + number + " в час");
+    }
+}
